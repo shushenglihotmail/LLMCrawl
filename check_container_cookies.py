83bd9bdfc7d18e5236bf0661e1a5750964c16069
Shusheng Li <sli@microsoft.com>
2025-12-08 17:15:14 -0800

    Remove debug scripts and reference files from root
    Delete one-off debugging utilities:
    - check_container_cookies.py
    - count_cookies.py
    - firecrawl_schema.sql

--- check_container_cookies.py
+++
@@ -1,7 +0,0 @@
-import json
-import sys
-
-data = json.load(sys.stdin)
-print(f'Cookies in container: {len(data["cookies"])}')
-for i, cookie in enumerate(data["cookies"], 1):
-    print(f'{i}. {cookie.get("name", "unnamed")} - {cookie.get("domain", "no domain")}')
